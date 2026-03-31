ce70f2b5cad169acfed0cdc638c5b2a143c10ff1
awehao <42124howard@gmail.com>
2026-03-31 15:46:30 +0800

    Initial commit: AMMR ROS2 workspace structure

---
+++ src/ammr_wholebody_mpc/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+package_name = 'ammr_wholebody_mpc'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [],
+    },
+)
