43bcb5ea385b11928a24ea84f9c0080e4c11b022
awehao <42124howard@gmail.com>
2026-04-15 18:54:58 +0800

    Auto-update: 2026-04-15 18:54

--- src/ammr_simulation/launch/gazebo.launch.py
+++ src/ammr_simulation/launch/gazebo.launch.py
@@ -34,6 +34,7 @@ def generate_launch_description():
                 '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
                 '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
                 '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
+                '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
                 '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
             ],
             output='screen',
