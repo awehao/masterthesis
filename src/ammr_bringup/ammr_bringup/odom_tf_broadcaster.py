a8288c7b7ba392d0e9e22b975acf89f5dc517ed8
awehao <42124howard@gmail.com>
2026-04-15 19:02:33 +0800

    Auto-update: 2026-04-15 19:02

--- src/ammr_bringup/ammr_bringup/odom_tf_broadcaster.py
+++ src/ammr_bringup/ammr_bringup/odom_tf_broadcaster.py
@@ -21,11 +21,21 @@ class OdomTfBroadcaster(Node):
     def __init__(self):
         super().__init__('odom_tf_broadcaster')
         self.br = TransformBroadcaster(self)
+        self._last_stamp = None
         self.create_subscription(Odometry, '/odom', self.odom_cb, BEST_EFFORT_QOS)
 
     def odom_cb(self, msg: Odometry):
+        stamp = msg.header.stamp
+        # Skip backwards timestamps to prevent "jump back in time" TF errors
+        if self._last_stamp is not None:
+            last_ns = self._last_stamp.sec * 1_000_000_000 + self._last_stamp.nanosec
+            curr_ns = stamp.sec * 1_000_000_000 + stamp.nanosec
+            if curr_ns <= last_ns:
+                return
+        self._last_stamp = stamp
+
         t = TransformStamped()
-        t.header.stamp = msg.header.stamp
+        t.header.stamp = stamp
         t.header.frame_id = 'odom'
         t.child_frame_id = 'ammr_base/base_footprint'
         t.transform.translation.x = msg.pose.pose.position.x
