76671b93e1b46240bd558c09fd1384cc6c6ce60c
awehao <42124howard@gmail.com>
2026-05-31 15:42:52 +0800

    Auto-update: 2026-05-31 15:42

--- src/ammr_bringup/launch/gazebo.launch.py
+++ src/ammr_bringup/launch/gazebo.launch.py
@@ -78,7 +78,7 @@ def generate_launch_description():
                 arguments=[
                     '-name', 'ammr_base',
                     '-topic', 'robot_description',
-                    '-x', '0', '-y', '0', '-z', '0.06',
+                    '-x', '0', '-y', '0', '-z', '0.14',
                 ],
                 output='screen',
             )],
